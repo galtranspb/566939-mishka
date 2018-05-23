a98e6170e7e0829bde97a1d74fa0c7743c111caf
AnatolySokolov <AnatoliySokolov1981@gmail.com>
2018-05-23 23:17:27 +0300

    Added script to open/close modal-window

--- source/js/main.js
+++ source/js/main.js
@@ -12,3 +12,21 @@ navToggle.addEventListener("click", function() {
     navMain.classList.remove("main-nav--opened");
   }
 });
+
+var modalButton = document.querySelector(".button--js");
+var modalPopup = document.querySelector(".modal");
+
+modalButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  modalPopup.classList.add("modal--show");
+
+});
+
+window.addEventListener("keydown", function (event) {
+  if (event.keyCode === 27) {
+    event.preventDefault();
+    if (modalPopup.classList.contains("modal--show")) {
+      modalPopup.classList.remove("modal--show");
+    }
+  }
+});
